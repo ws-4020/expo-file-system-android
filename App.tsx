186a488100600ef82cb1cc6d0753a92cc8cfcf76
creature-water-valley <83949308+creature-water-valley@users.noreply.github.com>
2022-12-02 19:34:16 +0900

    Add append option

--- App.tsx
+++ App.tsx
@@ -1,15 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import {Button, NativeModules, ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
 import {useCallback, useState} from "react";
 import * as FileSystem from 'expo-file-system';
-import { StorageAccessFramework } from 'expo-file-system';
+import {StorageAccessFramework} from 'expo-file-system';
 import {Dirs} from "react-native-file-access";
-import { NativeModules} from 'react-native'
+
 const { ManagePermission } = NativeModules;
 
 export default function App() {
   const [fileName, setFileName] = useState('');
-  const [dirName, setDirName] = useState('Measurement');
+  const [dirName, setDirName] = useState('');
   const requestDirectoryPermissions = useCallback(async () => {
     try {
       const safUri = StorageAccessFramework.getUriForDirectoryInRoot('Documents');
@@ -34,10 +33,9 @@ export default function App() {
     }
   }, [dirName])
 
-  const removeDirInDocuments = useCallback(async () => {
+  const removeInDocuments = useCallback(async () => {
     try {
-      const safUri = StorageAccessFramework.getUriForDirectoryInRoot('Documents');
-      const result = await StorageAccessFramework.deleteAsync(`${safUri}/${dirName}`);
+      const result = await StorageAccessFramework.deleteAsync(`content://com.android.externalstorage.documents/tree/primary%3ADocuments%2F${dirName}`);
     } catch (e) {
       console.log(e);
     }
@@ -54,10 +52,6 @@ export default function App() {
 
   const createDocumentsFile = useCallback(async () => {
     try {
-      const safUri = StorageAccessFramework.getUriForDirectoryInRoot('Documents');
-      // const result = await StorageAccessFramework.createFileAsync(`${safUri}/Measurement`, fileName, 'text/plain')
-      // content://com.android.externalstorage.documents/tree/primary:Documents/document/primary:Documents
-      // content://com.android.externalstorage.documents/tree/primary:Documents/Measurement
       const result = await StorageAccessFramework.createFileAsync(`content://com.android.externalstorage.documents/tree/primary%3ADocuments%2F${dirName}`, fileName, 'text/plain')
       console.log(result)
       await StorageAccessFramework.writeAsStringAsync(result, `info ${Date.now()}`)
@@ -68,11 +62,6 @@ export default function App() {
 
   const createMoviesFile = useCallback(async () => {
     try {
-      const safUri = StorageAccessFramework.getUriForDirectoryInRoot('Movies');
-      console.log(safUri)
-      // const result = await StorageAccessFramework.createFileAsync(`${safUri}/Measurement`, fileName, 'text/plain')
-      // content://com.android.externalstorage.documents/tree/primary:Documents/document/primary:Documents
-      // content://com.android.externalstorage.documents/tree/primary:Documents/Measurement
       const result = await StorageAccessFramework.createFileAsync(`content://com.android.externalstorage.documents/tree/primary%3AMovies%2F${dirName}`, fileName, 'video/mp4')
       await StorageAccessFramework.writeAsStringAsync(result, `info ${Date.now()}`)
     } catch (e) {
@@ -82,88 +71,79 @@ export default function App() {
 
   const writeDocumentsFile = useCallback(async () => {
     try {
-      // content://com.android.externalstorage.documents/tree/primary%3ADocuments%2FMeasurement/document/primary%3ADocuments%2FMeasurement%2F47.log.txt
-      await StorageAccessFramework.writeAsStringAsync(`content://com.android.externalstorage.documents/tree/primary%3ADocuments%2FMeasurement/document/primary%3ADocuments%2FMeasurement%2F47.log.txt`, `error error ${Date.now()}`)
+      await StorageAccessFramework.writeAsStringAsync(`content://com.android.externalstorage.documents/tree/primary%3ADocuments%2FMeasurement/document/primary%3ADocuments%2FMeasurement%2${fileName}`, `error error ${Date.now()}`)
     } catch (e) {
       console.log(e);
     }
   }, [fileName, dirName])
 
-  const getExternalDir = useCallback(() => {
+  const getSDCardDir = useCallback(() => {
     console.log(Dirs.SDCardDir)
-    console.log(Dirs.DocumentDir)
   }, [])
 
   const createDirInDocumentsWithFileSystem = useCallback(async () => {
     try {
-      // await FileSystem.makeDirectoryAsync(`file:///storage/emulated/0/Documents/BBAA`);
-      // const result = await FileSystem.getInfoAsync(`file:///storage/emulated/0/Documents`);
-      // const result = await FileSystem.writeAsStringAsync('file:///storage/emulated/0/Documents/test1.txt', 'test1');
-      // const result = await FileSystem.writeAsStringAsync('file:///storage/emulated/0/Movies/test1.txt', 'test1');
-      // const result = await FileSystem.makeDirectoryAsync(`/storage/emulated/0/Documents/BBAA`);
-      const result = await FileSystem.makeDirectoryAsync(`file:///sdcard/Documents/GGGG`);
-      // const result = await FileSystem.makeDirectoryAsync(`file:///sdcard/Documents/FFFF`);
-      console.log(result)
+      const result = await FileSystem.makeDirectoryAsync(`file:///sdcard/Documents/${dirName}`);
     } catch (e) {
       console.log(e)
     }
-  }, [getExternalDir])
+  }, [dirName])
 
   const writeFileInDocumentsWithFileSystem = useCallback(async () => {
     try {
-      // await FileSystem.makeDirectoryAsync(`file:///storage/emulated/0/Documents/BBAA`);
-      // const result = await FileSystem.getInfoAsync(`file:///storage/emulated/0/Documents`);
-      // const result = await FileSystem.writeAsStringAsync('file:///storage/emulated/0/Documents/test1.txt', 'test1');
-      // const result = await FileSystem.writeAsStringAsync('file:///storage/emulated/0/Movies/test1.txt', 'test1');
-      // const result = await FileSystem.makeDirectoryAsync(`/storage/emulated/0/Documents/BBAA`);
-      const result = await FileSystem.writeAsStringAsync(`file:///sdcard/Documents/GGGG/text.text`, 'test1');;
-      // const result = await FileSystem.makeDirectoryAsync(`file:///sdcard/Documents/FFFF`);
-      // console.log(result)
+      const result = await FileSystem.writeAsStringAsync(`file:///sdcard/Documents/${dirName}/${fileName}`, 'aaaa', {append: true});
     } catch (e) {
       console.log(e)
     }
-  }, [getExternalDir])
+  }, [dirName, fileName])
 
-  const getManagePermission = useCallback(async () => {
+  const getManageStoragePermission = useCallback(async () => {
     try {
       const result = await ManagePermission.getManageStoragePermission()
-      console.log(result)
     } catch (e) {
       console.log(e)
     }
   }, [])
 
-
-
   const requestManageStoragePermission = useCallback(async () => {
     try {
       const result = await ManagePermission.requestManageStoragePermission();
-      console.log(result)
     } catch (e) {
       console.log(e)
     }
   }, [])
 
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <TextInput onChangeText={text => setFileName(text)} value={fileName} placeholder={'please file name'}/>
-      <TextInput onChangeText={text => setDirName(text)} value={dirName} placeholder={'please dirName name'}/>
+      <TextInput onChangeText={text => setDirName(text)} value={dirName} placeholder={'please dir name'}/>
+
+      <View style={{height: 1, width: '100%', backgroundColor: 'grey'}}/>
+      <View style={{alignItems: 'flex-start'}}>
+        <Text>SAF</Text>
+      </View>
+      <View style={{height: 20}}/>
       <Button title={'Request dirName permission'} onPress={requestDirectoryPermissions}/>
       <View style={{height: 20}}/>
       <Button title={'Create dir in /Documents'} onPress={createDirInDocuments}/>
       <Button title={'Create file in /Documents'} onPress={createDocumentsFile}/>
-      <Button title={'Remove dir in /Documents'} onPress={removeDirInDocuments}/>
+      <Button title={'Remove in /Documents'} onPress={removeInDocuments}/>
+      <Button title={'Write file in /Documents'} onPress={writeDocumentsFile}/>
       <View style={{height: 20}}/>
       <Button title={'Create dir in /Movies'} onPress={createDirInMovies}/>
       <Button title={'Create file in /Movies'} onPress={createMoviesFile}/>
       <View style={{height: 20}}/>
-      <Button title={'Write file'} onPress={writeDocumentsFile}/>
-      <Button title={'Get external dir'} onPress={getExternalDir}/>
-      <Button title={'Create dir in /Documents with FileSystem'} onPress={createDirInDocumentsWithFileSystem}/>
-      <Button title={'get manage permission'} onPress={getManagePermission}/>
+      <View style={{height: 1, width: '100%', backgroundColor: 'grey'}}/>
+      <View style={{alignItems: 'flex-start'}}>
+        <Text>Non SAF</Text>
+      </View>
+      <View style={{height: 20}}/>
+      <Button title={'Create dir in /Documents non saf'} onPress={createDirInDocumentsWithFileSystem}/>
+      <Button title={'write file in /Documents non saf'} onPress={writeFileInDocumentsWithFileSystem}/>
+      <Button title={'get manage permission'} onPress={getManageStoragePermission}/>
       <Button title={'request manage permission'} onPress={requestManageStoragePermission}/>
-      <Button title={'write'} onPress={writeFileInDocumentsWithFileSystem}/>
-    </View>
+      <Button title={'Get sdcard dir'} onPress={getSDCardDir}/>
+    </ScrollView>
   );
 }
 
@@ -171,7 +151,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
   },
 });
