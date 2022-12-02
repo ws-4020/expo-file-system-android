4b87cabd37bd1ac4bd564d1f12c97936eec8042f
creature-water-valley <83949308+creature-water-valley@users.noreply.github.com>
2022-12-02 11:23:18 +0900

    wip

--- App.tsx
+++ App.tsx
@@ -1,9 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {useCallback} from "react";
+import * as FileSystem from 'expo-file-system';
 
 export default function App() {
+  const makeDocumentsDir = useCallback(async () => {
+    try {
+      await FileSystem.makeDirectoryAsync('Documents/Measurement');
+    } catch (e) {
+      console.log(e);
+    }
+  }, [])
+
   return (
     <View style={styles.container}>
+      <Button title={'Make dir'} onPress={makeDocumentsDir}/>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
